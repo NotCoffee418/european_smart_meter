631bfc76b6a0082930d09a4ab79b97830a35c123
Stijn Raeymaekers <9306304+NotCoffee418@users.noreply.github.com>
2025-10-15 19:39:43 +0200

    Revert "db structure for aggregate meter readings"
    This reverts commit 4f2f742a81d2263788929e27db8f77f2b1d7482c.

--- pkg/meterdb/type.go
+++ pkg/meterdb/type.go
@@ -4,9 +4,9 @@ type MeterDbPowerReadingType uint8
 
 const (
 	PowerConsumptionDay   MeterDbPowerReadingType = 0
-	PowerConsumptionNight MeterDbPowerReadingType = 1
-	PowerProductionDay    MeterDbPowerReadingType = 2
-	PowerProductionNight  MeterDbPowerReadingType = 3
+	PowerConsumptionNight                         = 1
+	PowerProductionDay                            = 2
+	PowerProductionNight                          = 3
 )
 
 type MeterDbLivePowerReading struct {
@@ -24,46 +24,4 @@ type MeterDbTotalPowerReading struct {
 type MeterDbTotalGasReading struct {
 	Timestamp           int64  `db:"timestamp"`
 	TotalConsumptionDM3 uint32 `db:"consumption_dm3"`
-}
-
-// Aggregate models - computed consumption deltas
-type AggregateLivePowerHourly struct {
-	HourStart          int64  `db:"hour_start"`
-	ConsumptionDayWh   uint32 `db:"consumption_day_wh"`
-	ConsumptionNightWh uint32 `db:"consumption_night_wh"`
-	ProductionDayWh    uint32 `db:"production_day_wh"`
-	ProductionNightWh  uint32 `db:"production_night_wh"`
-	SampleCount        uint32 `db:"sample_count"`
-}
-
-type AggregateLivePowerDaily struct {
-	DayStart           int64  `db:"day_start"`
-	ConsumptionDayWh   uint32 `db:"consumption_day_wh"`
-	ConsumptionNightWh uint32 `db:"consumption_night_wh"`
-	ProductionDayWh    uint32 `db:"production_day_wh"`
-	ProductionNightWh  uint32 `db:"production_night_wh"`
-	SampleCount        uint32 `db:"sample_count"`
-}
-
-type AggregateLivePowerMonthly struct {
-	MonthStart         int64  `db:"month_start"`
-	ConsumptionDayWh   uint32 `db:"consumption_day_wh"`
-	ConsumptionNightWh uint32 `db:"consumption_night_wh"`
-	ProductionDayWh    uint32 `db:"production_day_wh"`
-	ProductionNightWh  uint32 `db:"production_night_wh"`
-	SampleCount        uint32 `db:"sample_count"`
-}
-
-// Snapshot models - retained meter readings
-type SnapshotTotalPowerHourly struct {
-	Timestamp                int64  `db:"timestamp"`
-	ConsumptionDayStanding   uint32 `db:"consumption_day_standing"`
-	ConsumptionNightStanding uint32 `db:"consumption_night_standing"`
-	ProductionDayStanding    uint32 `db:"production_day_standing"`
-	ProductionNightStanding  uint32 `db:"production_night_standing"`
-}
-
-type SnapshotTotalGasHourly struct {
-	Timestamp   int64  `db:"timestamp"`
-	Dm3Standing uint32 `db:"dm3_standing"`
 }
